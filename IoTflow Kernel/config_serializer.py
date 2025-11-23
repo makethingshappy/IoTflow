fb27463be0aabddee3733b8906bc7310d369cd4c
Arshia Keshvari <110261434+TeslaNeuro@users.noreply.github.com>
2025-11-23 12:46:17 +0000

    Update config_serializer.py

--- IoTflow Kernel/config_serializer.py
+++ IoTflow Kernel/config_serializer.py
@@ -31,7 +31,7 @@
 
  Author: Arshia Keshvari
  Role: Independent Developer, Engineer, and Project Author
- Last Updated: 2025-11-16
+ Last Updated: 2025/11/23
 ==============================================================
 """
 
