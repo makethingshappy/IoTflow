f0dc369f5a80f813b98972ec7739b1f672077a5f
Arshia Keshvari <110261434+TeslaNeuro@users.noreply.github.com>
2025-11-23 12:11:42 +0000

    Update ads1x15.py
    License and third party license included

--- IoTflow Kernel/ads1x15.py
+++ IoTflow Kernel/ads1x15.py
@@ -1,3 +1,27 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2025 makethingshappy,
+#               2025 Arshia Keshvari (@TeslaNeuro)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 """
 ===========================================================
 ADS1x15 MicroPython Driver
@@ -8,9 +32,12 @@ analog-to-digital converters. Provides single-shot and continuous
 measurement modes, programmable gain control, comparator alert
 functionality, and voltage conversion helpers.
 
+This project includes third-party software (MIT Licensed) from 
+Radomir Dopieralski and Robert Hammelrath.
+
 Author: Arshia Keshvari
 Role: Independent Developer, Engineer, and Project Author
-Last Updated: 2025-11-16
+Last Updated: 2025-11-23
 """
 
 # The MIT License (MIT)
@@ -35,7 +62,6 @@ Last Updated: 2025-11-16
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-#
 
 import utime as time
 
