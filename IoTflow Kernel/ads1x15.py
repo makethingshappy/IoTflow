fa587a5a869d84ee2c9b0ed5d6c75acf60b46adc
Arshia Keshvari <110261434+TeslaNeuro@users.noreply.github.com>
2025-11-23 12:12:07 +0000

    Update ads1x15.py

--- IoTflow Kernel/ads1x15.py
+++ IoTflow Kernel/ads1x15.py
@@ -21,7 +21,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-
 """
 ===========================================================
 ADS1x15 MicroPython Driver
